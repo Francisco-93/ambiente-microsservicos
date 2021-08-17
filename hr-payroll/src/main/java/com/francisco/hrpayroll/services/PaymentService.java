730551860cbb5c010d830df041d7c3e90be3ad0a
francisco <francisco.aguiar@meta.com.br>
2021-08-17 13:53:58 -0300

    Comunicacao entre os microservices

--- hr-payroll/src/main/java/com/francisco/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/francisco/hrpayroll/services/PaymentService.java
@@ -1,13 +1,32 @@
 package com.francisco.hrpayroll.services;
 
 import com.francisco.hrpayroll.entities.Payment;
+import com.francisco.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    private RestTemplate restTemplate;
+
+    public PaymentService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     public Payment getPayment(Long workerId, int days){
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", String.valueOf(workerId));
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
 }
