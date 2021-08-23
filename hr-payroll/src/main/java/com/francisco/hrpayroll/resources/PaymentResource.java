8071c9810c06e798c6ef815664f9e4980098d8fd
francisco <francisco.aguiar@meta.com.br>
2021-08-23 17:32:55 -0300

    Configuracao Hystrix e timeout ribbon

--- hr-payroll/src/main/java/com/francisco/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/francisco/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.francisco.hrpayroll.resources;
 
 import com.francisco.hrpayroll.entities.Payment;
 import com.francisco.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,9 +19,15 @@ public class PaymentResource {
         this.service = service;
     }
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         Payment payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+        Payment payment = new Payment("Brennan", 400.0, days);
         return ResponseEntity.ok(payment);
     }
 }
